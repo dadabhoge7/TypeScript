f013e8b9160bab94127c12c86c232faf5f089f7a
dadabhoge7 <dadabhoge7@gmail.com>
2023-12-08 11:30:35 +0530

    add

--- 05_union.js
+++ 05_union.js
@@ -3,3 +3,23 @@ sn = "Dada";
 sn = 12345;
 sn = true;
 console.log(sn);
+// function print (arg : string | number){
+//     console.log(arg);
+//     if (typeof arg == 'number') {
+//         console.log(arg + 2);
+//     } else {
+//         console.log(arg+ " "+ 'test');
+//     }
+// }
+// print("CodeMind");
+// print(1234);
+function test(arg) {
+    if (typeof arg == 'number') {
+        console.log(arg + 2);
+    }
+    else {
+        console.log(arg + " " + 'Test');
+    }
+}
+test('CodeMind');
+test(2);
--- 05_union.ts
+++ 05_union.ts
@@ -3,3 +3,30 @@ sn = "Dada";
 sn = 12345;
 sn = true;
 console.log(sn);
+
+
+// function print (arg : string | number){
+//     console.log(arg);
+
+//     if (typeof arg == 'number') {
+//         console.log(arg + 2);
+        
+//     } else {
+//         console.log(arg+ " "+ 'test');
+//     }
+// }
+// print("CodeMind");
+// print(1234);
+
+function test(arg : number | string) : number | string {
+
+    if (typeof arg== 'number') {
+        console.log(arg + 2);
+        return 1;
+    } else {
+        console.log(arg + " " + 'Test');
+        return "";
+    }
+}
+test('CodeMind');
+test(2);
